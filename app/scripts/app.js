09e2dd211aae7dbc883d7e98e08a86b1e62fcfba
Daniel <daniel@coinport.com>
2014-12-08 18:36:15 +0800

    compiled version now works

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,6 +6,17 @@
     console.log('Polymer is ready to rock!');
   });
 
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
+
+  window.logAndContinue = function() {
+      var missing = Polymer.waitingFor();
+      if (missing.length) {
+        missing.forEach(function(el) {
+          console.warn('Waiting for: ' + el.getAttribute('name'));
+        });
+        console.warn('Forcing element registration.');
+        Polymer.forceReady();
+      }
+    };
+    // wrap document so it plays nice with other libraries
+    // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
