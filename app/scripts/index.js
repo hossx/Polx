53b6e647e04d056718f814f53bfecaff8d21a2e8
Daniel <daniel@coinport.com>
2015-03-04 00:37:18 +0800

    fix safari+zopim issue

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -37,46 +37,46 @@
     }
   };
 
-  if (/Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor)) {
-    setTimeout(function() {
-      (function(d, s) {
-        var z = window.$zopim = function(c) {
-          z._.push(c)
-        }
-        var x = z.s = d.createElement(s);
-        var e = d.getElementsByTagName(s)[0];
-        z.set = function(o) {
-          z.set._.push(o)
-        };
-        z._ = [];
-        z.set._ = [];
-        x.async = !0;
-        x.setAttribute('charset', 'utf-8');
-        x.src = '//v2.zopim.com/?2p7MBUmM53iXkyPP647DjIW7WeuknrJ1';
-        z.t = +new Date;
-        x.type = 'text/javascript';
-        e.parentNode.insertBefore(x, e);
-      })(document, 'script');
-
-      if (window.$zopim) {
-        window.$zopim(function() {
-          var z = window.$zopim;
-          if (window.lang === 'zh') {
-            z.livechat.setLanguage('zh_CN');
-            z.livechat.setGreetings({
-              'offline': '对不起，币丰港客服不在线上。请留言。',
-              'online': '客服MM在线，一起聊聊吧？'
-            });
-            z.livechat.concierge.setName('币丰港');
-            z.livechat.concierge.setTitle('有什么可以帮您的？');
-          } else {
-            z.livechat.setLanguage('en');
-            z.livechat.concierge.setTitle('How can I help you?');
-          }
-        });
+  //if (/Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor)) {
+  setTimeout(function() {
+    (function(d, s) {
+      var z = window.$zopim = function(c) {
+        z._.push(c)
       }
-    }, 2000);
-  }
+      var x = z.s = d.createElement(s);
+      var e = d.getElementsByTagName(s)[0];
+      z.set = function(o) {
+        z.set._.push(o)
+      };
+      z._ = [];
+      z.set._ = [];
+      x.async = !0;
+      x.setAttribute('charset', 'utf-8');
+      x.src = '//v2.zopim.com/?2p7MBUmM53iXkyPP647DjIW7WeuknrJ1';
+      z.t = +new Date;
+      x.type = 'text/javascript';
+      e.parentNode.insertBefore(x, e);
+    })(document, 'script');
+
+    if (window.$zopim) {
+      window.$zopim(function() {
+        var z = window.$zopim;
+        if (window.lang === 'zh') {
+          z.livechat.setLanguage('zh_CN');
+          z.livechat.setGreetings({
+            'offline': '对不起，币丰港客服不在线上。请留言。',
+            'online': '客服MM在线，一起聊聊吧？'
+          });
+          z.livechat.concierge.setName('币丰港');
+          z.livechat.concierge.setTitle('有什么可以帮您的？');
+        } else {
+          z.livechat.setLanguage('en');
+          z.livechat.concierge.setTitle('How can I help you?');
+        }
+      });
+    }
+  }, 1000);
+  // }
 
   // wrap document so it plays nice with other libraries
   // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
